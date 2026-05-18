94b3bb71acd2486cd8b0a02743bb9b69b6de30c4
KONG <120557690+KOINING@users.noreply.github.com>
2026-05-19 00:06:11 +0800

    服务器部署相关文件修改
    * 修改后端Mysql连接相关配置
    
    * 修改前端请求接口前缀
    
    * 修改接口转发规则
    
    * ...
    
    * 修改前端展示的平台名称

--- graduation-attainment-platform/frontend/src/views/home/HomeView.vue
+++ graduation-attainment-platform/frontend/src/views/home/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <el-card>
       <template #header>首页</template>
-      <p>欢迎使用高校教务管理系统</p>
+      <p>欢迎使用面向专业认证的毕业要求达成度统一计算平台</p>
     </el-card>
   </div>
 </template>
