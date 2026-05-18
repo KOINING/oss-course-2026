3d2eb09324cdcb360220f3a1cf4cf283b005c64c
PeSiCO <790601145@qq.com>
2026-05-18 14:06:49 +0800

    初始化前端项目

---
+++ src/main/java/com/oss/osscourse/OssCourseApplication.java
@@ -0,0 +1,13 @@
+package com.oss.osscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OssCourseApplication.class, args);
+    }
+
+}
