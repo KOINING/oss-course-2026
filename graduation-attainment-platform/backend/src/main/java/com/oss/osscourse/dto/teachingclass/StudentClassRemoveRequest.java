c52ee1c3aa5a316549b979a5f90daa24fdcd6ed5
maoxiaobin <692937482@qq.com>
2026-05-29 15:30:07 +0800

    feat: add all course list import api

---
+++ graduation-attainment-platform/backend/src/main/java/com/oss/osscourse/dto/teachingclass/StudentClassRemoveRequest.java
@@ -0,0 +1,14 @@
+package com.oss.osscourse.dto.teachingclass;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Schema(description = "学生-教学班关联移除请求")
+public class StudentClassRemoveRequest {
+
+    @NotNull(message = "关联ID不能为空")
+    @Schema(description = "学生-教学班关联ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long scId;
+}
