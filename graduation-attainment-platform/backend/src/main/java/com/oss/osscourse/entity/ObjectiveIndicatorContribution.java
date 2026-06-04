016d6c8b9521f5a550dce74c1698e3cacb600dd3
maoxiaobin <692937482@qq.com>
2026-06-04 14:18:23 +0800

    feat: add ObjectiveIndicatorContribution api

---
+++ graduation-attainment-platform/backend/src/main/java/com/oss/osscourse/entity/ObjectiveIndicatorContribution.java
@@ -0,0 +1,29 @@
+package com.oss.osscourse.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("objective_indicator_contribution")
+public class ObjectiveIndicatorContribution {
+    @TableId(value = "oic_id", type = IdType.AUTO)
+    private Long oicId;
+
+    private Long coId;
+
+    private Long ipId;
+
+    private Float internalWeight;
+
+    @TableField(value = "created_at", insertStrategy = FieldStrategy.NEVER, updateStrategy = FieldStrategy.NEVER)
+    private LocalDateTime createdAt;
+
+    @TableField(value = "updated_at", insertStrategy = FieldStrategy.NEVER, updateStrategy = FieldStrategy.NEVER)
+    private LocalDateTime updatedAt;
+}
